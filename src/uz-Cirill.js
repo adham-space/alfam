efcf3222befb9183ba9ed75da0079eacb6d573a8
Adham2023 <adham.muhammadjonov1923@gmail.com>
2021-09-26 20:35:28 +0500

    asd

---
+++ src/uz-Cirill.js
@@ -0,0 +1,120 @@
+export default {
+  el: {
+    colorpicker: {
+      confirm: 'Қабул қилиш',
+      clear: 'Тозалаш'
+    },
+    datepicker: {
+      now: 'Ҳозир',
+      today: 'Бугун',
+      cancel: 'Бекор қилиш',
+      clear: 'Тозалаш',
+      confirm: 'Қабул қилиш',
+      selectDate: 'Кунни танлаш',
+      selectTime: 'Соатни танлаш',
+      startDate: 'Бошланиш санаси',
+      startTime: 'Бошланиш вақти',
+      endDate: 'Тугаш санаси',
+      endTime: 'Тугаш вақти',
+      prevYear: 'Ўтган йил',
+      nextYear: 'Келгуси йил',
+      prevMonth: 'Ўтган ой',
+      nextMonth: 'Келгуси ой',
+      year: 'Йил',
+      month1: 'Январь',
+      month2: 'Февраль',
+      month3: 'Март',
+      month4: 'Апрель',
+      month5: 'Май',
+      month6: 'Июнь',
+      month7: 'Июль',
+      month8: 'Август',
+      month9: 'Сентабрь',
+      month10: 'Октабрь',
+      month11: 'Ноябрь',
+      month12: 'Декабрь',
+      week: 'Ҳафта',
+      weeks: {
+        sun: 'Як',
+        mon: 'Душ',
+        tue: 'Сеш',
+        wed: 'Чор',
+        thu: 'Пай',
+        fri: 'Жум',
+        sat: 'Шан'
+      },
+      months: {
+        jan: 'Ян',
+        feb: 'Фев',
+        mar: 'Мар',
+        apr: 'Апр',
+        may: 'Май',
+        jun: 'Июн',
+        jul: 'Июл',
+        aug: 'Авг',
+        sep: 'Сен',
+        oct: 'Окт',
+        nov: 'Ной',
+        dec: 'Дек'
+      }
+    },
+    select: {
+      loading: 'Юкланмоқда',
+      noMatch: 'Мос маълумот йўқ',
+      noData: 'Маълумот йўқ',
+      placeholder: 'Танладизнгиз'
+    },
+    cascader: {
+      noMatch: 'Мос маълумот топилмади',
+      loading: 'Юкланмоқда',
+      placeholder: 'Танлаш',
+      noData: 'Мос маълумот йўқ'
+    },
+    pagination: {
+      goto: 'Ўтиш',
+      pagesize: '/саҳифа',
+      total: 'Барчаси {total} та',
+      pageClassifier: ''
+    },
+    messagebox: {
+      title: 'Хабар',
+      confirm: 'Қабул қилиш',
+      cancel: 'Бекор қилиш',
+      error: 'Хатолик'
+    },
+    upload: {
+      deleteTip: 'Ўчириш тугмасини босиб оʻчирин',
+      delete: 'Ўчириш',
+      preview: 'Олдин кўриш',
+      continue: 'Давом қилиш'
+    },
+    table: {
+      emptyText: 'Бўш',
+      confirmFilter: 'Қабул қилиш',
+      resetFilter: 'Олдинги ҳолатга қайтариш',
+      clearFilter: 'Жами',
+      sumText: 'Суммаси'
+    },
+    tree: {
+      emptyText: 'Маълумот йўқ'
+    },
+    transfer: {
+      noMatch: 'Мос маълумот топилмади',
+      noData: 'Маълумот йўқ',
+      titles: ['1-жадвал', '2-жадвал'],
+      filterPlaceholder: 'Калит сўзни киритинг',
+      noCheckedFormat: '{total} та элемент',
+      hasCheckedFormat: '{checked}/{total} та белгиланди'
+    },
+    image: {
+      error: 'Хатолик'
+    },
+    pageHeader: {
+      title: 'Орқага'
+    },
+    popconfirm: {
+      confirmButtonText: 'Ҳа', // to be translated
+      cancelButtonText: 'Йўқ' // to be translated
+    }
+  }
+}
